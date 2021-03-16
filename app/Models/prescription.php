741c8a4daf452fe1656dca8b5057fe3319488861
Iftykhar <s.m.ifty49@gmail.com>
2021-03-17 05:22:36 +0600

    creating new Prescription model mcr

---
+++ app/Models/prescription.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class prescription extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'patient_name',
+        'patient_age',
+        'patient_gender',
+        'patient_weight',
+        'patient_bp_high',
+        'patient_bp_low'
+    ];
+}
