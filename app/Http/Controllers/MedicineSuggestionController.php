2ff6a95eddc01fa0a818dba273d7be56d81d748b
saiful29 <shaifulbd123@gmail.com>
2021-04-19 03:50:32 +0600

    clean up medicineSuggestion

--- app/Http/Controllers/MedicineSuggestionController.php
+++ app/Http/Controllers/MedicineSuggestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\MedicineSuggestion;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -12,18 +13,17 @@ class MedicineSuggestionController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
-        return MedicineSuggestion::all();
+        return response()->json(MedicineSuggestion::all());
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
      */
     public function create()
     {
@@ -31,7 +31,7 @@ class MedicineSuggestionController extends Controller
     }
 
 
-    public function store(Request $request): \Illuminate\Http\JsonResponse
+    public function store(Request $request): JsonResponse
     {
         //
        $validate = Validator::make($request->all(), [
@@ -48,49 +48,42 @@ class MedicineSuggestionController extends Controller
         if ($validate->fails()){
             return response()->json(['status' => false , 'message' => $validate->errors(), 'data' => $request]);
         }else{
-            return response()->json(['status' => true , 'message' => 'success']);
-
-        }
-        $med = new MedicineSuggestion;
-
-            $med->symptom_name = $request->symptom_name;
-            $med->medicine_name = $request->medicine_name;
-            $med->medicine_days = $request->medicine_days;
-            $med->medicine_morning = $request->medicine_morning;
-            $med->medicine_afternoon = $request->medicine_afternoon;
-            $med->medicine_evening = $request->medicine_evening;
-            $med->medicine_night = $request->medicine_night;
-            $med->medicine_continues = $request->medicine_continues;
-            $med->ms_score = 0;
-
-            $med->save();
+            MedicineSuggestion::create([
+                'symptom_name' => $request->input('symptom_name'),
+                'medicine_name' => $request->input('medicine_name'),
+                'medicine_days' => $request->input('medicine_days'),
+                'medicine_morning' => $request->input('medicine_morning'),
+                'medicine_afternoon' => $request->input('medicine_afternoon'),
+                'medicine_evening' => $request->input('medicine_evening'),
+                'medicine_night' => $request->input('medicine_night'),
+                'medicine_continues' => $request->input('medicine_continues'),
+                'medicine_score' => 0
+            ]);
             return response()->json(['status' => true, 'message' => 'data insert success']);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\MedicineSuggestion  $medicineSuggestion
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\MedicineSuggestion $medicineSuggestion
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(MedicineSuggestion $medicineSuggestion): \Illuminate\Http\JsonResponse
+    public function show(MedicineSuggestion $medicineSuggestion): JsonResponse
     {
-        //
         try{
-            $med = MedicineSuggestion::findOrfail($medicineSuggestion->id);
+             return response()->json(MedicineSuggestion::findOrFail($medicineSuggestion->id));
         }
         catch (ModelNotFoundException $exception){
             return response()->json(['Not found'], 404);
         }
-        return $med;
-
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\MedicineSuggestion  $medicineSuggestion
-     * @return \Illuminate\Http\Response
+     * @return void
      */
     public function edit(MedicineSuggestion $medicineSuggestion)
     {
@@ -100,11 +93,11 @@ class MedicineSuggestionController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\MedicineSuggestion  $medicineSuggestion
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\MedicineSuggestion $medicineSuggestion
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, MedicineSuggestion $medicineSuggestion): \Illuminate\Http\JsonResponse
+    public function update(Request $request, MedicineSuggestion $medicineSuggestion): JsonResponse
     {
         //
         $validate = Validator::make($request->all(), [
@@ -118,40 +111,37 @@ class MedicineSuggestionController extends Controller
             'medicine_continues' => 'required',
         ]);
 
-        //update
         if($validate->fails()){
             return response()->json($validate->errors());
         }else {
             try {
                 $med = MedicineSuggestion::findOrfail($medicineSuggestion->id);
+                $med->update([
+                    'symptom_name' => $request->input('symptom_name'),
+                    'medicine_name' => $request->input('medicine_name'),
+                    'medicine_days' => $request->input('medicine_days'),
+                    'medicine_morning' => $request->input('medicine_morning'),
+                    'medicine_afternoon' => $request->input('medicine_afternoon'),
+                    'medicine_evening' => $request->input('medicine_evening'),
+                    'medicine_night' => $request->input('medicine_night'),
+                    'medicine_continues' => $request->input('medicine_continues'),
+                    'medicine_score' => 0
+                ]);
+                return response()->json(['status' => true, 'message' => 'update Success']);
             } catch (ModelNotFoundException $exception) {
                 return response()->json('not found model');
             }
         }
-
-        $med->symptom_name = $request->symptom_name;
-        $med->medicine_name = $request->medicine_name;
-        $med->medicine_days = $request->medicine_days;
-        $med->medicine_morning = $request->medicine_morning;
-        $med->medicine_afternoon = $request->medicine_afternoon;
-        $med->medicine_evening = $request->medicine_evening;
-        $med->medicine_night = $request->medicine_night;
-        $med->medicine_continues = $request->medicine_continues;
-        $med->ms_score = 0;
-
-        $med->save();
-        return response()->json('done');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\MedicineSuggestion  $medicineSuggestion
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\MedicineSuggestion $medicineSuggestion
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(MedicineSuggestion $medicineSuggestion): \Illuminate\Http\JsonResponse
+    public function destroy(MedicineSuggestion $medicineSuggestion): JsonResponse
     {
-        //
         MedicineSuggestion::destroy($medicineSuggestion->id);
         return response()->json(['status' => true, 'message' => 'delete success']);
     }
